1cb9b18683cd6da723a27cf58cbc9afd148dd9cd
fahrizaasril <84886351+fahrizaasril@users.noreply.github.com>
2021-05-27 13:16:32 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,17 +16,46 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+
+state = 0
+bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
+        `masukan nilai i|v contohnya 6|9`
     );   
+    state = 1;
 });
+
+bot.on('message', (msg) => {
+    if(state == 1){
+        console.log(msg.text);
+        s = msg.text.split("|")
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            console.log(jres);
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang diprediksi adalah ${jres[0]} volt`
+            );
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
+            );
+        })
+    }else{
+        state = 0
+    }
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
